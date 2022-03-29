162a25b053eca011ef10604b447fbb139458a28b
Gonzaga Móveis <gonzagamoveis.pt@gmail.com>
2022-03-30 00:38:25 +0100

    Alteração URL

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://app-bb9df65c-c904-4e2e-80e1-ee95fb961bac.cleverapps.io/'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),

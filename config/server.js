a0714a24a09baef1646077a88ddfafffe80cd053
Gonzaga Móveis <gonzagamoveis.pt@gmail.com>
2022-03-30 23:09:15 +0100

    Alteração porta

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'https://app-bb9df65c-c904-4e2e-80e1-ee95fb961bac.cleverapps.io/'),
-  port: env.int('PORT', 8080),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },

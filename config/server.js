68b59b07bb0d53bbe59d7e6996842785bf779e3b
Gonzaga Móveis <gonzagamoveis.pt@gmail.com>
2022-03-31 00:52:07 +0100

    Remover URL

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env.int('URL', 'http://localhost:1337'),
   app: {
     keys: env.array("APP_KEYS", ['BLjkzQaw/l0dMiEYxC6Omw==','6JAw0AM21O9jlMEtOBTvTQ==','ourZnV5bzZmhlVg6eiKXMw==','yyaiHROjkMx3CZGZdaK7kg==']),
   },

8f6360975d5dce929b4479df4c233b1600006daa
Gonzaga Móveis <gonzagamoveis.pt@gmail.com>
2022-03-31 01:06:29 +0100

    Alterar URL

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  serveAdminPanel: false,
+  url: env('ADMIN_URL', ''),
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'cbe13ca4-7f3e-45e8-b166-06cb6cf1b9be'),
   },

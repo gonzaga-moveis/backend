e0e953461fd58e456962be1e643ad3848ebe7ec4
Gonzaga Móveis <gonzagamoveis.pt@gmail.com>
2022-06-18 23:20:13 +0100

    Alterações BD

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('MYSQL_ADDON_HOST', 'localhost'),
+      host: env('MYSQL_ADDON_HOST', 'bhtvq6pocdo3qeoqgbwv-mysql.services.clever-cloud.com'),
       port: env.int('MYSQL_ADDON_PORT', 3306),
-      database: env('MYSQL_ADDON_DB', 'backend'),
-      user: env('MYSQL_ADDON_USER', 'root'),
-      password: env('MYSQL_ADDON_PASSWORD', 'password'),
+      database: env('MYSQL_ADDON_DB', 'bhtvq6pocdo3qeoqgbwv'),
+      user: env('MYSQL_ADDON_USER', 'uwplrci3prwdvllg'),
+      password: env('MYSQL_ADDON_PASSWORD', 'kR0xUDR8GhGrz2UhPOZz'),
       ssl: env.bool('DATABASE_SSL', false),
     },
     pool: {
